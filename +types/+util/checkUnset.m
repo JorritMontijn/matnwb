1c7cce71f793cad2496e0fba2273cc4d1cd0e3f0
JorritMontijn <jorritmontijn@gmail.com>
2020-06-19 13:10:22 +0200

    Update checkUnset.m

--- +types/+util/checkUnset.m
+++ +types/+util/checkUnset.m
@@ -1,21 +1,21 @@
 function checkUnset(obj, argin)
-props = properties(obj);
-anonNames = {};
-for i=1:length(props)
-    p = obj.(props{i});
-    if isa(p, 'types.untyped.Anon')
-        anonNames = [anonNames;{p.name}];
-    elseif isa(p, 'types.untyped.Set')
-        anonNames = [anonNames;keys(p) .'];
+	props = properties(obj);
+	anonNames = {};
+	for i=1:length(props)
+		p = obj.(props{i});
+		if isa(p, 'types.untyped.Anon')
+			anonNames = [anonNames;{p.name}];
+		elseif isa(p, 'types.untyped.Set')
+			anonNames = [anonNames;keys(p) .'];
+		end
 	end
-end
-if any(contains(argin,'unit'))
-	anonNames = [anonNames;argin{contains(argin,'unit')}];
-end
-
-dropped = setdiff(argin, [props;anonNames])
-if ~isempty(dropped)
-    error('Properties {%s} are not valid property names.',...
-        misc.cellPrettyPrint(dropped));
-end
+% 	if any(contains(argin,'unit'))
+% 		anonNames = [anonNames;argin{contains(argin,'unit')}];
+% 	end
+% 	
+	dropped = setdiff(argin, [props;anonNames]);
+	if ~isempty(dropped)
+		error('Properties {%s} are not valid property names.',...
+			misc.cellPrettyPrint(dropped));
+	end
 end
